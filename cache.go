dd03a34219acfb1e6c99ca0d05825f0246ba3c4a
motemen <motemen@gmail.com>
2013-11-07 20:05:57 +0900

    initial version

---
+++ cache.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"github.com/golang/glog"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+	"time"
+)
+
+type Cache struct {
+	Root string
+}
+
+type CacheEntry struct {
+	FilePath string
+	os.FileInfo
+}
+
+func (cache *Cache) UrlToFilePath(url *url.URL) string {
+	s := []string{cache.Root, url.Host}
+	s = append(s, strings.Split(url.Path, "/")...)
+	return path.Join(s...)
+}
+
+func (cache *Cache) Get(url *url.URL) (string, error) {
+	filePath := cache.UrlToFilePath(url)
+	bytes, err := ioutil.ReadFile(filePath)
+	return string(bytes), err
+}
+
+func (cache *Cache) Set(url *url.URL, content string) error {
+	filePath := cache.UrlToFilePath(url)
+	dir, _ := path.Split(filePath)
+
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filePath, []byte(content), 0666)
+}
+
+func (cache *Cache) GetWriter(url *url.URL) (io.Writer, error) {
+	filePath := cache.UrlToFilePath(url)
+	glog.Infof("GetWriter: %s -> %s", url, filePath)
+
+	dir, _ := path.Split(filePath)
+
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		return nil, err
+	}
+
+	return os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
+}
+
+func (cache *Cache) GetEntry(url *url.URL) *CacheEntry {
+	filePath := cache.UrlToFilePath(url)
+	fileInfo, _ := os.Stat(filePath)
+	return &CacheEntry{filePath, fileInfo}
+}
+
+func (cacheEntry *CacheEntry) Content() ([]byte, error) {
+	return ioutil.ReadFile(cacheEntry.FilePath)
+}
+
+func (cacheEntry *CacheEntry) GetWriter() (io.WriteCloser, error) {
+	glog.Infof("[%s] GetWriter", cacheEntry.FilePath)
+
+	dir, _ := path.Split(cacheEntry.FilePath)
+
+	if err := os.MkdirAll(dir, 0777); err != nil {
+		return nil, err
+	}
+
+	return os.OpenFile(cacheEntry.FilePath, os.O_WRONLY|os.O_CREATE, 0666)
+}
+
+func (cacheEntry *CacheEntry) SetMtime(mtime time.Time) error {
+	glog.Infof("[%s] Setting mtime to %s", cacheEntry.FilePath, mtime)
+	return os.Chtimes(cacheEntry.FilePath, mtime, mtime)
+}
+
+func (cacheEntry *CacheEntry) Exists() bool {
+	return cacheEntry.FileInfo != nil
+}
---
+++ etch.go
@@ -0,0 +1,254 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/elazarl/goproxy"
+	"github.com/golang/glog"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type EtchProxy struct {
+	goproxy.ProxyHttpServer
+	Cache *Cache
+}
+
+type EtchContextData struct {
+	CachedContent *bytes.Buffer
+}
+
+type BufioReadCloser struct {
+	bufio.Reader
+	io.Closer
+}
+
+func NewBufioReadCloser(rc io.ReadCloser) *BufioReadCloser {
+	return &BufioReadCloser{*bufio.NewReader(rc), rc}
+}
+
+func ReqMethodIs(method string) goproxy.ReqConditionFunc {
+	return func(req *http.Request, ctx *goproxy.ProxyCtx) bool {
+		return req.Method == method
+	}
+}
+
+func StatusCodeIs(code int) goproxy.RespCondition {
+	return goproxy.RespConditionFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) bool {
+		if resp == nil {
+			return false
+		}
+		return resp.StatusCode == code
+	})
+}
+
+type RequestMutex struct {
+	sync.Mutex
+	resChans map[string][]chan *http.Response
+}
+
+var requestMutex = &RequestMutex{resChans: make(map[string][]chan *http.Response)}
+
+func (proxy *EtchProxy) GuardRequest(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	requestMutex.Lock()
+	chans, ok := requestMutex.resChans[req.URL.String()]
+
+	if ok {
+		ch := make(chan *http.Response)
+		requestMutex.resChans[req.URL.String()] = append(chans, ch)
+		requestMutex.Unlock()
+
+		glog.V(3).Infof("[%s] Waiting for ongoing request", req.URL)
+
+		res := <-ch
+
+		glog.V(3).Infof("[%s] Response got from chan: %s", req.URL, res)
+
+		if res != nil {
+			return req, res
+		}
+	} else {
+		requestMutex.resChans[req.URL.String()] = make([]chan *http.Response, 0)
+		requestMutex.Unlock()
+	}
+
+	return req, nil
+}
+
+func (proxy *EtchProxy) MakeRangedRequest(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	cache := proxy.Cache
+
+	if entry := cache.GetEntry(req.URL); entry.FileInfo != nil {
+		glog.Infof("%s: found cache entry", req.URL)
+
+		content, err := entry.Content()
+		if err != nil {
+			glog.Errorf("OnRequest: retrieving cache content: %s", err)
+			return req, nil
+		}
+
+		// TODO mutex
+		cachedContent := bytes.NewBuffer(content)
+		req.Header.Add("Range", fmt.Sprintf("bytes=%d-", cachedContent.Len()-1))
+		req.Header.Add("If-Modified-Since", entry.FileInfo.ModTime().Format(time.RFC850))
+
+		_, resp, err := proxy.Tr.DetailedRoundTrip(req)
+		if err != nil {
+			glog.Errorf("OnRequest: executing request: %s", err)
+			return req, nil
+		}
+
+		if resp.StatusCode == 416 {
+			glog.Infof("[%s] Got 416: attempting re-fetch", req.URL)
+
+			// clear cache
+			req.Header.Del("Range")
+			req.Header.Del("If-Modified-Since")
+
+			_, _resp, err := proxy.Tr.DetailedRoundTrip(req)
+
+			if err != nil {
+				glog.Errorf("OnRequest: re-fetch: %s", err)
+				return req, nil
+			}
+
+			resp = _resp
+			cachedContent = new(bytes.Buffer)
+		}
+
+		ctx.UserData = &EtchContextData{cachedContent}
+
+		return req, resp
+	}
+
+	return req, nil
+}
+
+func (proxy *EtchProxy) RestoreCache(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+	glog.V(3).Infof("[%s] Headers: %s", ctx.Req.URL, resp.Header)
+
+	if ctx.UserData == nil {
+		return resp
+	}
+
+	userData := ctx.UserData.(*EtchContextData)
+
+	switch resp.StatusCode {
+	case 200:
+		// nop
+
+	case 206:
+		buf := userData.CachedContent
+
+		// TODO check Content-Range value
+		contentRange := resp.Header.Get("Content-Range")
+		glog.V(2).Infof("Content-Range: %s", contentRange)
+
+		// 1 バイトだけキャッシュと重複してるはずなので
+		// その部分を整合性チェックに使う
+		responseBody := bufio.NewReader(resp.Body)
+		firstByte, err := responseBody.ReadByte()
+
+		if err != nil {
+			glog.Errorf("[%s] Reading response: %s", ctx.Req.URL, err)
+			return goproxy.NewResponse(
+				ctx.Req, goproxy.ContentTypeText, 500, fmt.Sprintf("Reading response: %s", err))
+			}
+
+			if buf.Bytes()[buf.Len()-1] != firstByte {
+				// TODO re-attempt
+				glog.Errorf("[%s] Cache mismatch", ctx.Req.URL)
+				return resp
+			}
+
+			io.Copy(buf, responseBody)
+
+			resp.StatusCode = 200
+			resp.Header.Del("Content-Range")
+			resp.Body = ioutil.NopCloser(buf)
+
+		case 304:
+			resp.StatusCode = 200
+			resp.Body = ioutil.NopCloser(userData.CachedContent)
+
+		default:
+			glog.Errorf("[%s] Unhandled status code: %d", ctx.Req.URL, resp.StatusCode)
+		}
+
+		return resp
+}
+
+func (proxy *EtchProxy) StoreCache (resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+	cache := proxy.Cache
+
+	lastModified := time.Now()
+	lastModifiedString := resp.Header.Get("Last-Modified")
+	if lastModifiedString != "" {
+		var err error
+		lastModified, err = http.ParseTime(lastModifiedString)
+		if err != nil {
+			glog.Errorf(`[%s]: Parsing Last-Modified header "%s": %s`, ctx.Req.URL, lastModifiedString, err)
+		}
+	}
+
+	cacheEntry := cache.GetEntry(ctx.Req.URL)
+	if cacheEntry.FileInfo == nil || cacheEntry.FileInfo.ModTime().Before(lastModified) {
+		glog.Infof("[%s] update cache: %s", ctx.Req.URL, cacheEntry.FilePath)
+
+		cacheWriter, err := cacheEntry.GetWriter()
+		if err != nil {
+			glog.Errorf("[%s]: cacheEntry.GetWriter: %s", ctx.Req.URL, err)
+			return resp
+		}
+
+		buf := new(bytes.Buffer)
+		io.Copy(io.MultiWriter(buf, cacheWriter), resp.Body)
+		cacheWriter.Close()
+
+		cacheEntry.SetMtime(lastModified)
+
+		resp.Body = ioutil.NopCloser(buf)
+	} else {
+		glog.V(2).Infof("[%s] Response is not fresher than cache: %s <= %s", ctx.Req.URL, lastModified, cacheEntry.FileInfo.ModTime())
+	}
+
+	return resp
+}
+
+func (proxy *EtchProxy) UnguardRequest (resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+	requestMutex.Lock()
+	defer requestMutex.Unlock()
+	if chans := requestMutex.resChans[ctx.Req.URL.String()]; chans != nil {
+		for _, ch := range chans {
+			ch <- resp
+		}
+		delete(requestMutex.resChans, ctx.Req.URL.String())
+	}
+
+	return resp
+}
+
+func main() {
+	cacheDir := flag.String("cache-dir", "cache", "cache directory")
+	proxyPort := flag.Int("port", 8080, "proxy port")
+
+	flag.Parse()
+
+	cache := &Cache{*cacheDir}
+	proxy := &EtchProxy{*goproxy.NewProxyHttpServer(), cache}
+
+	proxy.OnRequest(ReqMethodIs("GET")).DoFunc(proxy.GuardRequest)
+	proxy.OnRequest(ReqMethodIs("GET")).DoFunc(proxy.MakeRangedRequest)
+	proxy.OnResponse(ReqMethodIs("GET")).DoFunc(proxy.RestoreCache)
+	proxy.OnResponse(goproxy.ContentTypeIs("text/plain"), ReqMethodIs("GET"), StatusCodeIs(200)).DoFunc(proxy.StoreCache)
+	proxy.OnResponse().DoFunc(proxy.UnguardRequest)
+
+	proxy.Verbose = true
+
+	glog.Errorf("%s", http.ListenAndServe(fmt.Sprintf(":%d", *proxyPort), proxy))
+}
