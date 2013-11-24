1edf0ad80a2b2482a119c221f10d82e303951d15
motemen <motemen@gmail.com>
2013-11-24 22:06:45 +0900

    deleting cache

--- control.go
+++ control.go
@@ -5,6 +5,8 @@ import (
 	"github.com/elazarl/goproxy"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
 )
 
 type EtchControlServer struct {
@@ -61,8 +63,51 @@ func setupControlMux(proxy *EtchProxy) {
 		rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
 
 		keys := proxy.Cache.Keys()
-		for _, key := range(keys) {
+		for _, key := range keys {
 			rw.Write([]byte(key.String()))
+		}
+	})
+
+	proxy.ControlMux.HandleFunc("/cache", func(rw http.ResponseWriter, req *http.Request) {
+		urlString := req.URL.Query().Get("url")
+		if urlString == "" {
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		u, err := url.Parse(urlString)
+		if err != nil {
+			rw.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		cacheEntry := proxy.Cache.GetEntry(u)
+		content, mtime, err := cacheEntry.GetContent()
+
+		if os.IsNotExist(err) {
+			rw.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		switch req.Method {
+		case "HEAD":
+			rw.Header().Set("Last-Modified", mtime.Format(http.TimeFormat))
+
+		case "GET":
+			rw.Header().Set("Last-Modified", mtime.Format(http.TimeFormat))
+			rw.Write(content)
+
+		case "DELETE":
+			if err := cacheEntry.Delete(); err != nil {
+				errorf(proxy, "Deleting cache %s: %s", cacheEntry, err)
+				rw.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+
+			rw.WriteHeader(http.StatusNoContent)
+
+		default:
+			rw.WriteHeader(http.StatusMethodNotAllowed)
 		}
 	})
 }
