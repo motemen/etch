fb308d29b8c9e19e509d78c6afb73649eefd707e
motemen <motemen@gmail.com>
2013-12-23 23:44:42 +0900

    eg: Broker class

--- eg/elasticsearch-broker.rb
+++ eg/elasticsearch-broker.rb
@@ -20,34 +20,59 @@ class Post
   end
 end
 
-etch_origin = ARGV.shift || 'http://localhost:25252'
-es_origin   = ARGV.shift || 'http://localhost:9200'
+class Broker
+  def initialize(etch_origin: 'http://localhost:25252', es_origin: 'http://localhost:9200')
+    @logger = Logger.new(STDERR)
+    @logger.level = Logger::INFO
 
-logger = Logger.new(STDERR)
-logger.level = Logger::INFO
+    @etch = Faraday.new url: etch_origin
+    @es   = Elasticsearch::Client.new url: es_origin, logger: @logger
+  end
 
-etch = Faraday.new url: etch_origin
-es   = Elasticsearch::Client.new url: es_origin, logger: logger
+  def fetch_all_thread_urls
+    @logger.info('fetching all thread urls...')
+    @etch.get('/').body.each_line.map { |url| url.chomp }
+  end
 
-etch.get('/').body.each_line do |url|
-  url.chomp!
-
-  logger.info("#{url}...")
-
-  posts = etch.get('/cache', { url: url }).body.each_line.with_index.map do |post, i|
-    begin
-      Post.new(url, i+1, post)
-    rescue => e
-      logger.error "#{url} at #{i+1}: #{e}"
-      nil
+  def index_all_urls!
+    fetch_all_thread_urls.each do |url|
+      index_thread_posts!(url)
     end
-  end.compact
+  end
 
-  begin
-    es.bulk body: posts.map { |p|
-      { index: { _index: 'etch', _type: 'post', _id: p.id, data: { name: p.name, mail: p.mail, meta: p.meta, body_html: p.body, title: p.title } } }
-    } .to_a
-  rescue => e
-    logger.error "#{url}: #{e}"
+  def index_thread_posts!(url)
+    @logger.info("indexing #{url}...")
+
+    posts = @etch.get('/cache', { url: url }).body.each_line.with_index.map do |post, i|
+      begin
+        Post.new(url, i+1, post)
+      rescue => e
+        @logger.error "#{url} at #{i+1}: #{e}"
+        nil
+      end
+    end.compact
+
+    begin
+      @es.bulk body: posts.map { |p|
+        { index: { _index: 'etch', _type: 'post', _id: p.id, data: { name: p.name, mail: p.mail, meta: p.meta, body_html: p.body, title: p.title } } }
+      } .to_a
+    rescue => e
+      @logger.error "#{url}: #{e}"
+    end
   end
 end
+
+config = {}
+
+OptionParser.new do |opts|
+  opts.on('--etch http://localhost:25252', 'etch origin') do |o|
+    config[:etch_origin] = o
+  end
+
+  opts.on('--es http://localhost:9200', 'ElasticSearch origin') do |o|
+    config[:es_origin] = o
+  end
+end.parse!
+
+broker = Broker.new(config)
+broker.index_all_urls!
